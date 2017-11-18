11b3f4f3e3b6318bf8d800d27ea6003546ca9534
Vinh Nguyen <ntvinh.11586@gmail.com>
2017-11-18 17:07:02 +0700

    Add basic calculator

---
+++ android-support-algorithm/src/main/java/com/ntvinh11586/android_support_algorithm/BasicIntegerCalculation.java
@@ -0,0 +1,20 @@
+package com.ntvinh11586.android_support_algorithm;
+
+public class BasicIntegerCalculation {
+
+    public static Integer addTwoIntegers(Integer a, Integer b) {
+        return a + b;
+    }
+
+    public static Integer subtractTwoIntegers(Integer a, Integer b) {
+        return a - b;
+    }
+
+    public static Integer multiplyTwoIntegers(Integer a, Integer b) {
+        return a * b;
+    }
+
+    public static Integer divideTwoIntegers(Integer a, Integer b) {
+        return a / b;
+    }
+}
